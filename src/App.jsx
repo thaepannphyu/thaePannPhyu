436e23bc978b50ee152b3c407cfd3ed68b117906
PhyoWunnaLin <phyowunnalin1214@gmail.com>
2023-07-22 14:18:57 +0630

    add git

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./App.css";
+
+const App = () => {
+  return (
+    <div>
+      <h1>hello</h1>
+      <section>
+        {/* sections components */}
+      </section>
+    </div>
+  )
+}
+
+export default App
+
