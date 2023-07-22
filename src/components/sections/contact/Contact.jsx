2c22967a783d7ec13dc6c8eb58e2b9e4cf333546
PhyoWunnaLin <phyowunnalin1214@gmail.com>
2023-07-22 23:09:38 +0630

    stop

--- src/components/sections/contact/Contact.jsx
+++ src/components/sections/contact/Contact.jsx
@@ -2,11 +2,15 @@ import React from "react";
 
 const Contact = () => {
   return (
-    <div className="bg-[#F7F7FF] flex justify-center w-[100%]">
+    <div className="bg-[#F7F7FF] dark:bg-[#0E0E17] flex justify-center w-[100%]">
       <div className="w-[90%] my-20 flex justify-between">
         {/* left info */}
-        <div className="w-[30%] flex flex-col">
-            <h1 className=" text-black text-4xl font-bold">What’s your story? Get in touch</h1>
+        <div className="w-[30%] flex flex-col gap-3">
+            <h1 className=" text-black dark:text-white text-4xl font-medium">What’s your story? Get in touch</h1>
+            <p className=" text-gray-800 dark:text-gray-300 font-extralight">Always available for freelancing if the right project comes along, Feel free to contact me.</p>
+            <div className="flex flex-col gap-3">
+
+            </div>
         </div>
       </div>
     </div>
