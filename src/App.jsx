d540dd576cd99033f4a63b888d5b3dadeffa47c5
PhyoWunnaLin <phyowunnalin1214@gmail.com>
2023-07-23 14:36:38 +0630

    contact finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import "./App.css";
 import Contact from './components/sections/contact/Contact';
+import Footer from './components/footer/Footer';
 
 const App = () => {
   return (
     <div>
-        <Contact/>
+
     </div>
   )
 }
