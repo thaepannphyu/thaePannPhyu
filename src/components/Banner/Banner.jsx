af5ab1610c050be7cda15a974d21c03ae25dce68
khinezinthin <khinezinthinwk1@gmail.com>
2023-07-25 11:11:58 +0630

    recent banner

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -4,10 +4,10 @@ const Banner = ({ title }) => {
   return (
     <div>
       <div className=" select-none">
-        <h1 className={`relative text-[#0B0B13] dark:text-[#FFFFFF] text-3xl tracking-wide font-medium`}>
+        <span className={`relative text-[#0B0B13] dark:text-[#FFFFFF] text-3xl tracking-wide font-medium`}>
           {title}
         <div className=" h-12 w-12 bg-[#525ee153] rounded-[50%] absolute circleMove top-0 left-[-14px]"> </div>
-        </h1>
+        </span>
       </div>
     </div>
   );
